74512b74a9c2c12d73af0ba0b192f428d0f87c82
Daniel.Bello.Rodríguez <bellorodriguezdanielalejandro@gmail.com>
2026-06-26 17:13:45 -0500

    se incorporo dotenv y se creo la funcion crear

--- Evidencia GA7_220501096_AA2-EV01_Codificación_de_modulos _del_software/bace-de-datos.js
+++ Evidencia GA7_220501096_AA2-EV01_Codificación_de_modulos _del_software/bace-de-datos.js
@@ -1,2 +1,21 @@
 require("dotenv").config();
 const { Pool } = require("pg");
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
+
+const formatoColombiano = new Intl.NumberFormat("es-CO", {
+  style: "currency",
+  currency: "COP",
+  minimumFractionDigits: 0,
+});
+
+async function crear(name, description, image, price) {
+  const resultado = await pool.query(
+    'INSERT INTO public."Productos" (name, description, image, price) VALUES ($1, $2, $3, $4) RETURNING *',
+    [name, description, image, price],
+  );
+  return resultado.rows[0];
+}
